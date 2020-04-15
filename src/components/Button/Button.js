703ad2cbac7dec2b8cd215b084d63ed0e7d7afce
Anath <anathkantonda@gmail.com>
2020-04-15 12:54:05 +0200

    Added files

---
+++ src/components/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Random/style.css';
+
+class Button extends React.Component {
+	render() {
+		return (
+			<button
+				className={ this.props.light ? 'light-button' : 'dark-button' }
+        onClick={this.props.onClick} >
+				Refresh
+			</button>
+		);
+	}
+}
+
+export default Button;
